1ceccf864b02598df10d4af18800a7e3a2eac3e9
Matthew Harris <mattswim17@gmail.com>
2020-10-16 21:35:58 -0600

    a thing

--- ui.R
+++ ui.R
@@ -1,33 +1,57 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+shinyUI(dashboardPage(skin='green',
+    dashboardHeader(title = 'Starbucks'),
+    dashboardSidebar(sidebarMenu(
+        menuItem('Introduction', tabName = 'intro', icon = icon('info-circle')),
+        menuItem('US Store Map', tabName = 'stores', icon = icon('globe-americas')),
+        menuItem('Income vs Coffee', tabName = 'income', icon = icon('dollar-sign')),
+        menuItem('About Me', tabName = 'about', icon = icon('user-circle'))
+    )),
+    dashboardBody(tabItems(
+        tabItem(tabName = 'intro',
+                fluidPage(
+                    fluidRow(column(offset = 2, width = 8, align = 'center', h1(tags$b(tags$u("Starbucks Store Placement"))))),
+                ),
+                fluidPage(
+                    fluidRow(column(offset = 2, width = 8, align = 'center', h3("Income per Capita vs. Expansion in the US"))),
+                    
+                ),
+                br(),
+                br(),
+                fluidPage(
+                    fluidRow(column(offset = 2, width = 8, align = 'left', 
+                                    box(width = 12, h2(tags$u(tags$b('Hypothesis'))), background = 'olive',
+                                        p("It is easy to theorize that income per capita would have a positive correlation with the 
+                                          number of Starbucks stores in a given state, that Starbucks corporate would want to place
+                                          stores near populations with higher disposable incomes and refrain from placing stores 
+                                          near populations with lower disposable incomes.")
+                                        ))),
+                )),
+        tabItem(tabName = 'about',
+                fluidPage(
+                    br(),
+                    fluidRow(column(offset = 2, width = 8, align= 'center', img(src = "elbert1.jpg", height = 200))), 
+                    br(),
+                    br(),
+                    fluidRow(column(offset = 2, width = 8, align = 'center',
+                                    box(width = 12, h2(tags$u(tags$b('Matthew Harris'))), background = 'olive',
+                        p("I graduated with undergraduate degrees in Business Administration and Business Economics in the
+                          spring of 2011.  For nearly the next decade, I struggled to find a job that fulfilled both my 
+                          interests and curiosity about the world."),
+                        br(),
+                        p("It's of no great surprise that I ended up back in the world of data modeling, this time,
+                          incorporating coding techniques rather than the excel based modeling tools I had used in college. 
+                          I am currently studying data science at the New York City Data Science Academy and laying the
+                          foundations in R, Python, SQL, and machine learning that will allow me to pursue a career in 
+                          data science in the coming year."),
+                        br(),
+                        p("In my spare time, I frequent the gym, and as a resident of Denver, I am looking forward to
+                          once again being too busy for snowboarding this winter season."),
+                        br(),
+                        br(),
+                    ))
+                        
+                    )
+                    
+                ))
+    ))
 ))
